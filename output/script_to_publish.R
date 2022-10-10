fd9a58de0f9431fb8c69d749fb6870cde8d81a8e
Benjamin Meyer <ben@KWF-Rugged-01.KWF.LOCAL>
2022-10-09 20:30:41 -0800

    commit

--- output/script_to_publish.R
+++ output/script_to_publish.R
@@ -10,3 +10,6 @@ render_book()
 
 # publish new version
 publish_book(account = "kwfwqx", name = "Miller_Creek_Vogel_Lake_WQX")
+
+# terminal command to stage all commits: 
+# git add -A && git commit -m 'staging all files'
